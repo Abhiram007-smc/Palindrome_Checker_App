6dfd338664eb57f76bd50e6091d34a007a191c92
Abhiram007-smc <abhiram.pitani@gmail.com>
2026-02-25 15:19:47 +0530

    ul5-plaindrone

--- APP/src/PalindroneCheckerApp.java
+++ APP/src/PalindroneCheckerApp.java
@@ -1,30 +1,59 @@
+
+import java.util.Stack;
+
+/**
+ * =========================================================
+ * MAIN CLASS – UseCase5PalindromeCheckerApp
+ * =========================================================
+ *
+ * Use Case 5: Stack Based Palindrome Checker
+ *
+ * Description:
+ * This class validates a palindrome using a Stack
+ * data structure which follows the LIFO principle.
+ *
+ * At this stage, the application:
+ * - Pushes characters into a stack
+ * - Pops them in reverse order
+ * - Compares with original sequence
+ * - Displays the result
+ *
+ * This maps stack behavior to reversal logic.
+ *
+ * @author Pavitra
+ * @version 5.0
+ */
+
 public class PalindroneCheckerApp {
 
+    /**
+     * Application entry point for UC5.
+     *
+     * @param args Command-line arguments
+     */
     public static void main(String[] args) {
 
         // Declare and initialize the input string
-        String input = "radar";
+        String input = "noon";
 
-        // Convert the string into a character array
-        char[] chars = input.toCharArray();
+        // Create a Stack to store characters
+        Stack<Character> stack = new Stack<>();
 
-        // Initialize pointers
-        int start = 0;
-        int end = chars.length - 1;
+        // Push each character of the string into the stack
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
 
         // Assume palindrome initially
         boolean isPalindrome = true;
 
-        // Continue comparison until pointers cross
-        while (start < end) {
+        // Iterate again through original string and compare with popped values
+        for (char c : input.toCharArray()) {
 
-            if (chars[start] != chars[end]) {
+            if (c != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
-
-            start++;
-            end--;
         }
 
         System.out.println("Input : " + input);
